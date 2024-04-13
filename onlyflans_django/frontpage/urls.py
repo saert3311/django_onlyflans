ecf74a9ad7a3877f751f74b6bacba6845cdf0be4
Jose Duarte <saert@outlook.com>
2024-04-13 16:17:56 -0400

    Update templates with header, footer, and navbar, also adding favicons

--- onlyflans_django/frontpage/urls.py
+++ onlyflans_django/frontpage/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import *
 
@@ -5,4 +7,4 @@ app_name = 'frontpage'
 
 urlpatterns = [
     path('', index_view, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- onlyflans_django/frontpage/views.py
+++ onlyflans_django/frontpage/views.py
@@ -1,6 +1,9 @@
 from operator import index
 from django.views.generic import TemplateView
 
+#tengo el presentimiento que mas adelante veremos vistas de clases asi que me adelanto un poco para
+#no tener que hacer tantos cambios mas adelante
+
 class IndexView(TemplateView):
     template_name = 'index.html'
 
