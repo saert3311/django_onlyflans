51ab251308cd4c1ce7835573fcf00633a0b535a9
Jose Duarte <saert@outlook.com>
2024-04-12 21:47:11 -0400

    Estructurar templates usando herencia

--- onlyflans_django/frontpage/urls.py
+++ onlyflans_django/frontpage/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import *
 
 app_name = 'frontpage'
 
 urlpatterns = [
-
+    path('', index_view, name='index'),
 ]
--- onlyflans_django/frontpage/views.py
+++ onlyflans_django/frontpage/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from operator import index
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page'] = 'Index'
+        return context
+    
+index_view = IndexView.as_view()
