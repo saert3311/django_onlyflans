1830379b4557fb3a32416ac11b4bcd5be3121e2f
Jose Duarte <saert@outlook.com>
2024-04-17 19:09:59 -0400

    Flan model, migration, and admin register

--- onlyflans_django/frontpage/models.py
+++ onlyflans_django/frontpage/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class Flan(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    image = models.URLField()
+    is_private = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.pk}: {self.name}'
